19e2174c57343dac19675e41395bb2ee22badd7f
Honorata92 <h.sajdak1@o2.pl>
2022-05-29 00:21:00 +0200

    add method

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Lab6
 {
@@ -7,30 +8,76 @@ namespace Lab6
     {
         private static void Main(string[] args)
         {
-            List<User> users = new List<User>();
-            users.Add(new User() { Name = "Adam", Role = "ADMIN", Age = 35, Marks = null, CreatedAt = new DateTime(2000, 01, 20) });
-            users.Add(new User() { Name = "Elżbieta", Role = "ADMIN", Age = 40, Marks = null, CreatedAt = new DateTime(2022, 01, 20) });
-            users.Add(new User() { Name = "Marek", Role = "ADMIN", Age = 50, Marks = null, CreatedAt = new DateTime(2020, 06, 20) });
+            //Ilość rekordów w tablicy
+            var count = Users.Get().Count();
+            //Console.WriteLine(count);
+            //Listę nazw użytkowników ( nazwa zmiennej)
+            var names = Users.Get().Select(u => u.Name);
+            /*foreach (var a in names)
+            {
+                Console.Write(a + " ");
+            }*/
 
-            users.Add(new User() { Name = "Stefan", Role = "MODERATOR", Age = 50, Marks = null, CreatedAt = new DateTime(2010, 01, 20) });
-            users.Add(new User() { Name = "Róża", Role = "MODERATOR", Age = 65, Marks = null, CreatedAt = new DateTime(20020, 04, 20) });
-            users.Add(new User() { Name = "Grzegorz", Role = "MODERATOR", Age = 55, Marks = null, CreatedAt = new DateTime(2022, 03, 20) });
-            users.Add(new User() { Name = "Andrzej", Role = "MODERATOR", Age = 45, Marks = null, CreatedAt = new DateTime(2022, 04, 20) });
+            //Posortowanych użytkowników po nazwach
+            var usersByName = Users.Get().OrderBy(u => u.Name);
+            /*foreach (var item in usersByName)
+            {
+                Console.WriteLine(item.Name);
+            }
+            */
+            //Listę dostępnych ról użytkowników
+            var roles = Users.Get().Select(u => u.Role).Distinct();
+            /*foreach (var a in roles)
+            {
+                Console.Write(a + " ");
+            }*/
+            //Listy pogrupowanych użytkowników po rolach
+            var orderByRoles = Users.Get().GroupBy(u => u.Role);
 
-            users.Add(new User() { Name = "Erwin", Role = "TEACHER", Age = 35, Marks = null, CreatedAt = new DateTime(2020, 06, 20), RemovedAt = new DateTime(2022, 06, 20) });
-            users.Add(new User() { Name = "Aleksandra", Role = "TEACHER", Age = 55, Marks = null, CreatedAt = new DateTime(2020, 01, 20) });
-            users.Add(new User() { Name = "Erwin", Role = "TEACHER", Age = 30, Marks = null, CreatedAt = new DateTime(2020, 06, 20) });
+            //Ilość rekordów, dla których podano oceny(nie null i więcej niż 0)
+            var degree = Users.Get().Where(u => u.Marks == null).Count();
 
-            users.Add(new User() { Name = "Dominik", Role = "STUDENT", Age = 15, Marks = new int[] { 1, 3, 5 }, CreatedAt = new DateTime(2020, 06, 20) });
-            users.Add(new User() { Name = "Nina", Role = "STUDENT", Age = 15, Marks = new int[] { 1, 5, 5 }, CreatedAt = new DateTime(2020, 06, 20) });
-            users.Add(new User() { Name = "Dawid", Role = "STUDENT", Age = 10, Marks = new int[] { 5, 5, 5 }, CreatedAt = new DateTime(2020, 06, 20) });
-            users.Add(new User() { Name = "Marcin", Role = "STUDENT", Age = 11, Marks = new int[] { 3, 2, 3 }, CreatedAt = new DateTime(2020, 06, 20) });
-            users.Add(new User() { Name = "Damian", Role = "STUDENT", Age = 16, Marks = new int[] { 5, 5, 3 }, CreatedAt = new DateTime(2020, 06, 20) });
-            users.Add(new User() { Name = "Jolanta", Role = "STUDENT", Age = 12, Marks = new int[] { 5, 5, 2 }, CreatedAt = new DateTime(2020, 06, 20) });
-            users.Add(new User() { Name = "Sara", Role = "STUDENT", Age = 12, Marks = new int[] { 3, 5, 3 }, CreatedAt = new DateTime(2020, 06, 20) });
-            users.Add(new User() { Name = "Kamila", Role = "STUDENT", Age = 6, Marks = new int[] { 1, 4, 4 }, CreatedAt = new DateTime(2020, 06, 20) });
-            users.Add(new User() { Name = "Dawid", Role = "STUDENT", Age = 14, Marks = new int[] { 5, 4, 3 }, CreatedAt = new DateTime(2020, 06, 20) });
-            users.Add(new User() { Name = "Paulina", Role = "STUDENT", Age = 10, Marks = new int[] { 4, 4, 2 }, CreatedAt = new DateTime(2020, 06, 20) });
+            //Sumę, ilość i średnią wszystkich ocen studentów
+            var countDegree = Users.Get().Where(u => u.Marks != null).SelectMany(u => u.Marks).Count();
+            //Console.WriteLine(countDegree);
+            var sumDegree = Users.Get().Where(u => u.Marks != null).SelectMany(u => u.Marks).Sum();
+            //Console.WriteLine(sumDegree);
+            var avgDegree = Users.Get().Where(u => u.Marks != null).SelectMany(u => u.Marks).Average();
+            //Console.WriteLine((double)117 / 33);
+            //Console.WriteLine(avgDegree);
+
+            //Najlepszą ocenę
+            var maxDegree = Users.Get().Where(u => u.Marks != null).SelectMany(u => u.Marks).Max();
+            //Console.WriteLine(maxDegree);
+
+            //Najgorszą ocenę
+            var minDegree = Users.Get().Where(u => u.Marks != null).SelectMany(u => u.Marks).Min();
+            //Console.WriteLine(minDegree);
+
+            //Najlepszego studenta
+            var ListStudent1 = Users.Get().OrderBy(u => u.Name).Where(u => u.Marks != null).ToList();
+            var ListStudent2 = ListStudent1.OrderBy(u => u.Marks.Average()).ToList();
+            var best = ListStudent2.Select(u => u.Name).Last();
+            Console.WriteLine(best);
+            //Listę studentów, którzy posiadają najmniej ocen
+            //var ListStudent3 = ListStudent1.OrderBy(u => u.Marks).Max().ToString();
+            //Console.WriteLine(best);
+
+            //Listę studentów, którzy posiadają najwięcej ocen
+
+            //Listę obiektów zawierających tylko nazwę i średnią ocenę(mapowanie na inny obiekt)
+
+            //Studentów posortowanych od najlepszego
+            var ListStudent3 = ListStudent1.OrderByDescending(u => u.Marks.Average()).ToList();
+            Console.WriteLine(ListStudent3);
+            //Listę użytkowników pogrupowanych po miesiącach daty utworzenia(np. 2022 - 02, 2022 - 03, 2022 - 04, itp.)
+
+            //var groupByMonth = Users.Get().GroupBy(u => u.CreatedAt);
+
+            //Listę użytkowników, którzy nie zostali usunięci(data usunięcia nie została ustawiona)
+            //var noDeleteStudent = Users.Get().OrderBy
+
+            //Najnowszego studenta(najnowsza data utworzenia)
         }
     }
 }
